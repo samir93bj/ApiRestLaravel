a23fb79348238972b3e3e5d438ee3f713c8b4c6f
samir93bj <samir93bj@gmail.com>
2021-10-13 20:35:34 -0300

    Actualizacion del Proyecto

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,14 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
+
+Route::get('/articulos','App\Http\Controllers\ArticuloController@index'); //MOSTRAMOS TODOS LOS REGISTROS
+Route::post('/articulos/create','App\Http\Controllers\ArticuloController@store');    //ALMACENAMOS EL REGISTRO
+Route::put('/articulos/{id}','App\Http\Controllers\ArticuloController@update'); //ACTUALIZAMOS EL REGISTRO
+Route::delete('/articulos/{id}','App\Http\Controllers\ArticuloController@destroy'); //ELIMINAMOS EL REGISTRO
+
